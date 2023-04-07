5497f19ddd2714d10e41bb85be8a00ce1123e265
Swapy78 <swapnilbabanmane@gmail.com>
2023-04-07 21:31:59 +0530

    Added Java program to check equality of two lines

---
+++ LinesEquality.java
@@ -0,0 +1,20 @@
+package demo;
+
+public class LinesEquality {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 int a1 = 1, b1 = 2, c1 = 3, a2 = 4, b2 = 5, c2 = 6;
+		           
+		 if ((a1 / a2 == b1 / b2) && (a1 / a2 == c1 / c2) && (b1 / b2 == c1 / c2))
+			        
+			    System.out.println( "The lines are equal");
+			        
+			 
+			    else
+			        System.out.println("The lines are not equal");
+			          
+			} 
+	}
+
+
